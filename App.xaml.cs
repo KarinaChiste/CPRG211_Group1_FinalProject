04d4fc7e597924282dddea4c784a36fc40d5aa84
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-04-10 08:22:44 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CPRG211_Group1_FinalProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "CPRG211_Group1_FinalProject" };
+        }
+    }
+}
