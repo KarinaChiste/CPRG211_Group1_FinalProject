effc5c94441e92849436b2d0afd45dbdd82a4d7e
Caden-O <caden.ortt@edu.sait.ca>
2025-04-17 06:54:36 -0600

    added orders to master

---
+++ Classes/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211_Group1_FinalProject.Classes
+{
+    internal class Order
+    {
+        //itemsOrdered will be the names of each item ordered seperated by a comma
+        public Order(int orderNum,
+                        string itemNum,
+                        string itemName,
+                        int quantity)
+        {
+        }
+    }
+}
