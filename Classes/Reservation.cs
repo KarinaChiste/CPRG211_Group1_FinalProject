0a25ee4f48a28f128ff3d3209ca208abf141727a
Ashtonrp <ashton.pritchard@edu.sait.ca>
2025-04-16 22:10:25 -0400

    Adds Reservaiton class Adds ReservationManager class

---
+++ Classes/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211_Group1_FinalProject.Classes
+{
+    public class Reservation
+    {
+        public string Name { get; set; }
+        public string GroupSize { get; set; }
+        public string Time {  get; set; }
+
+        public Reservation(string name, string groupSize, string time)
+        {
+            Name = name;
+            GroupSize = groupSize;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Group Size: {GroupSize}, {Time}";
+        }
+    }
+}
