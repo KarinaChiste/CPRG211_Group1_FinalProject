a4610e771708c70481cf6573ebc52c8ad965d540
Ashtonrp <ashton.pritchard@edu.sait.ca>
2025-04-23 23:07:46 -0400

    Adds date and ID to reservation class fixes remove reservation header

--- Classes/Reservation.cs
+++ Classes/Reservation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,17 +12,22 @@ namespace CPRG211_Group1_FinalProject.Classes
         public string Name { get; set; }
         public string GroupSize { get; set; }
         public string Time {  get; set; }
+        public string Date { get; set; }
+        public string ReservationID { get; set; }
 
-        public Reservation(string name, string groupSize, string time)
+        public Reservation(string name, string groupSize, string time, string date, string reservationID)
         {
             Name = name;
             GroupSize = groupSize;
             Time = time;
+            Date = date;
+            ReservationID = reservationID;
+
         }
 
         public override string ToString()
         {
-            return $"{Name}, Group Size: {GroupSize}, {Time}";
+            return $"{Name}, Group Size: {GroupSize}, {Time}, {Date}, {ReservationID}";
         }
     }
 }
