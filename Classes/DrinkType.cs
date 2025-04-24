5f0d883d6726ba5b6f7f8b1e9cca658a3e5ada05
sinferna <jaspritgrewal@hotmail.com>
2025-04-23 21:53:44 -0600

    - added logic to MenuItem.cs and MenuManager.cs - created and added logic to AppetizerType.cs, MainType.cs, SideType.cs, DessertType.cs, DrinkType.cs

---
+++ Classes/DrinkType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211_Group1_FinalProject.Classes
+{
+    class DrinkType : MenuItem
+    {
+        public DrinkType(string itemId, string itemName, string price, string itemType) : base(itemId, itemName, price, itemType)
+        {
+        }
+
+        public override string GetItemType()
+        {
+            return "Drink";
+        }
+    }
+}
--- Classes/MenuItem.cs
+++ Classes/MenuItem.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CPRG211_Group1_FinalProject.Exceptions;
 
 namespace CPRG211_Group1_FinalProject.Classes
 {
-    class MenuItem
+    public abstract class MenuItem
     {
+        private string itemId;
+        private string itemName;
+        private string price;
+        private string itemType;
+
+        public string ItemId { get => itemId; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new EmptyFieldException("Item ID:");
+                }
+                itemId = value;
+            } 
+        }
+
+        public string ItemName { get => itemName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new EmptyFieldException("Item ID:");
+                }
+                itemName = value;
+            }
+        }
+
+        public string Price { get => price;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new EmptyFieldException("Item ID:");
+                }
+                price = value;
+            }
+        }
+
+        public string ItemType { get; set; }
+
+        public MenuItem(string itemId, string itemName, string price, string itemType)
+        {
+            ItemId = itemId;
+            ItemName = itemName;
+            Price = price;
+            ItemType = itemType;
+        }
+
+        public override string ToString()
+        {
+            return $"{ItemId}, {ItemName}, {Price}, {ItemType}";
+        }
+
+        public abstract string GetItemType();
+
     }
 }
